1c3d296c942e9fa43f5fd8a554bd51ed94218048
Tanvir Islam Streame <streametanvir@gmail.com>
2019-05-02 09:41:20 +0600

    Update urls.py

--- employeemanagement/employeemanagement/urls.py
+++ employeemanagement/employeemanagement/urls.py
@@ -15,11 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from employee.views import StudentInfo,Studentdetail,filterGender
+from employee.views import StudentInfo,Studentdetail
 from django.conf.urls import url
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^api/v1/all', StudentInfo),
     url(r'^api/v1/Studentdetail/(?P<pk>\d+)/$', Studentdetail),
-    url(r'^api/v1/filterGender/(?P<gendername>[\w\-]+)/$', filterGender),
 ]
